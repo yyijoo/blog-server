8366488abd8825d16701e0de0a3f29a1e88aad33
yyijoo <yyijoo@gmail.com>
2019-05-08 14:12:19 +0900

    Add router to test connection

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.get("/testconnection", function(req, res) {
+  res.send("Hello World!");
+});
+
 app.listen(port, () => {
   console.log(`Express server has started on ${port}`);
 });
