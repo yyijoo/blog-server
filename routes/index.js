3473317aaa7eb61f1e270711198d8a5999bce2a4
yyijoo <yyijoo@gmail.com>
2019-06-07 20:46:22 +0900

    feat: Add post api for adding til

--- routes/index.js
+++ routes/index.js
@@ -3,25 +3,50 @@ var router = express.Router();
 var bodyParser = require("body-parser"); // client의 post 요청에서 body의 parameter를 쉽게 추출함.
 router.use(bodyParser.urlencoded({ extended: true }));
 const Til = require("../models/til");
+const Blog = require("../models/blog");
 
 var showdown = require("showdown");
 var converter = new showdown.Converter();
 
-// router.post("/api/addtils", (req, res) => {
-//   Til.create(
-//     {
-//       week: "11",
-//       startDate: "190101",
-//       endDate: "190101",
-//       content: "test"
-//     },
-//     function(err, results) {
-//       if (err) return handleError(err);
-//       res.json(results);
-//       // saved!
-//     }
-//   );
-// });
+router.post("/api/addtils", (req, res) => {
+  console.log("req.bodyyyyy", req.body, JSON.stringify(req.body));
+
+  res.header("Access-Control-Allow-Origin", "*");
+
+  const til = new Til();
+  til.week = 1;
+  til.startDate = "190101";
+  til.endDate = "190105";
+  til.content = JSON.stringify(req.body);
+  console.log("til", til);
+
+  til.save(error => {
+    if (error) {
+      console.log(error);
+      res.json({ result: 0 });
+      return;
+    }
+
+    res.json({ result: 1 });
+  });
+
+  // Til.create(
+  //   {
+  //     week: "11",
+  //     startDate: "190101",
+  //     endDate: "190101",
+  //     content: req.body
+  //   },
+  //   function(err, results) {
+  //     if (err) console.log(err);
+
+  //     res.json(results);
+  //     // saved!
+  //   }
+  // );
+});
+
+// router.get("/api/blog/search")
 
 router.get("/api/til", (req, res) => {
   Til.find({}, function(err, result) {
